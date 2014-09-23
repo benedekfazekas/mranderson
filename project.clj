621256a85207c8e05f9dc171eba456a4e6972ea5
Benedek Fazekas <benedek.fazekas@gmail.com>
2014-09-23 11:23:36 +0100

    getting ready for 0.2.1 release
    - as i messed up 0.2.0

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject thomasa/mranderson "0.2.0"
+(defproject thomasa/mranderson "0.2.1"
   :description "Leiningen plugin to download and use some dependencies as source."
   :url "https://github.com/benedekfazekas/mranderson"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
-  :plugins [[thomasa/mranderson "0.2.0"]]
+  :plugins [[thomasa/mranderson "0.2.1"]]
   :dependencies [^:source-dep [com.cemerick/pomegranate "0.3.0"]
                  ^:source-dep [org.clojure/tools.namespace "0.2.5"]
                  ^:source-dep [me.raynes/fs "1.4.4"]])
