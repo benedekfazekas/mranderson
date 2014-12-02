a2b35e08b66a2b06babe16462994db5ddf499bc7
Benedek Fazekas <benedek.fazekas@mailonline.co.uk>
2014-12-02 18:30:05 +0000

    bumps up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thomasa/mranderson "0.2.2"
+(defproject thomasa/mranderson "0.3.0-SNAPSHOT"
   :description "Leiningen plugin to download and use some dependencies as source."
   :url "https://github.com/benedekfazekas/mranderson"
   :license {:name "Eclipse Public License"
