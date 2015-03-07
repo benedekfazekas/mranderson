b4ba1652eee0b23430a378c33cc2338ac6d4359a
Benedek Fazekas <benedek.fazekas@gmail.com>
2015-03-07 19:37:02 +0000

    mranderson uses latest mranderson

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
-  :plugins [[thomasa/mranderson "0.3.0"]]
+  :plugins [[thomasa/mranderson "0.3.1"]]
   :dependencies [^:source-dep [com.cemerick/pomegranate "0.3.0"]
                  ^:source-dep [org.clojure/tools.namespace "0.2.7"]
                  ^:source-dep [me.raynes/fs "1.4.4"]])
