edd08dfd80a3cb3c6de6b894c8e9dab4d4f2ab15
Benedek Fazekas <benedek.fazekas@gmail.com>
2016-02-28 22:08:56 +0000

    Prepare release 0.4.7

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ function check_result {
     fi
 }
 
-lein do clean, source-deps :skip-javaclass-repackage true :project-prefix mranderson047snapshot
+lein do clean, source-deps :skip-javaclass-repackage true :project-prefix mranderson047
 check_result
 lein with-profile plugin.mranderson/config "$@"
