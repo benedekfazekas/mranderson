f6c3fd307e496207ce9e6dba8ecb68db6c0db91f
Benedek Fazekas <benedek.fazekas@gmail.com>
2021-02-18 11:32:52 +0000

    bump version for next dev cycle

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject thomasa/mranderson "0.5.3"
+(defproject thomasa/mranderson "0.5.4-SNAPSHOT"
   :description "Dependency inlining and shadowing tool."
   :url "https://github.com/benedekfazekas/mranderson"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in :leiningen
-  :plugins [[thomasa/mranderson "0.5.2"]]
+  :plugins [[thomasa/mranderson "0.5.3"]]
   :java-source-paths ["java-src"]
   :javac-options ["-target" "1.6" "-source" "1.6"]
   :filespecs [{:type :bytes :path "mranderson/project.clj" :bytes ~(slurp "project.clj")}]
