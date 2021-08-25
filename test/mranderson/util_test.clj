694545d50eb27ff0d9c772b2a329201212c119ab
Benedek Fazekas <benedek.fazekas@gmail.com>
2021-08-25 12:15:38 +0100

    Revert "filter out `clojure.lang` package"
    This reverts commit 55b190e07b59cedb47bf826848a0b9d60fd3794a.

--- test/mranderson/util_test.clj
+++ test/mranderson/util_test.clj
@@ -1,20 +1,9 @@
 (ns mranderson.util-test
-  (:require [clojure.test :as t]
-            [mranderson.util :as u]))
+  (:require [mranderson.util :as util]
+            [clojure.test :as t]))
 
-(t/deftest removes-subdirs-test
+(t/deftest test-removes-subdirs
   (t/testing "handles subdirs where one dir-name is a prefix of another"
-    (t/are [expected dirs] (t/is (= expected (u/remove-subdirs dirs)))
+    (t/are [expected dirs] (t/is (= expected (util/remove-subdirs dirs)))
       ["hiccup"] ["hiccup/foo" "hiccup"]
       ["hiccup" "hiccup2"] ["hiccup/foo" "hiccup" "hiccup2"])))
-
-(t/deftest ->package-names-test
-  (let [class-names ["foo.bar.PersistentMapProxy$IEquality"
-                     "foo.bar.BazPersistentMapProxy$IEquality"
-                     "foo.bar.baz.PersistentMapProxy$IEquality"
-                     "bar.goo.BigClass"]
-        expected-packages #{"foo.bar" "foo.bar.baz" "bar.goo"}]
-    (t/testing "class names turned into package names"
-      (t/is (= expected-packages (u/->package-names class-names))))
-    (t/testing "clojure.lang is filtered out"
-      (t/is (= expected-packages (u/->package-names (cons "clojure.lang.PersistentUnrolledVector$Card1$1" class-names)))))))
