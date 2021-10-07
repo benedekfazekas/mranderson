b9cc0aa8ce2b0d1acb1695512eeb2b8f28c84caa
vemv <vemv@users.noreply.github.com>
2021-10-07 10:37:27 +0200

    Introduce a CI matrix

--- project.clj
+++ project.clj
@@ -6,7 +6,10 @@
   :eval-in :leiningen
   :plugins [[thomasa/mranderson "0.5.3"]]
   :java-source-paths ["java-src"]
-  :javac-options ["-target" "1.6" "-source" "1.6"]
+  :javac-options ~(cond
+                    (re-find #"^17." (System/getProperty "java.version"))
+                    ["-target" "1.7" "-source" "1.7"]
+                    :else ["-target" "1.6" "-source" "1.6"])
   :filespecs [{:type :bytes :path "mranderson/project.clj" :bytes ~(slurp "project.clj")}]
   :dependencies [^:inline-dep [com.cemerick/pomegranate "0.4.0"]
                  ^:inline-dep [org.clojure/tools.namespace "1.1.0"]
@@ -17,7 +20,7 @@
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
                                   [leiningen-core "2.9.1"]]}
              :eastwood {:dependencies [[org.clojure/clojure "1.10.3"]]
-                        :plugins      [[jonase/eastwood "0.9.1"]]
+                        :plugins      [[jonase/eastwood "0.9.9"]]
                         :eastwood     {:exclude-linters [:no-ns-form-found]}}
              :kaocha {:dependencies [[lambdaisland/kaocha "0.0-418"]
                                      [lambdaisland/kaocha-cloverage "0.0-32"]]}}
