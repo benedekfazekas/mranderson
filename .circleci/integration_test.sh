d032b441c63b251d8814ff8cdf9c45a0f116a7e5
vemv <vemv@users.noreply.github.com>
2021-10-21 20:58:47 +0200

    `unset CI`

--- .circleci/integration_test.sh
+++ .circleci/integration_test.sh
@@ -16,6 +16,8 @@ lein clean
 # Undo the patch if it was applied already:
 git checkout project.clj
 git apply ../update-mranderson.patch
+# cider-nrepl observes CI, which triggers :pedantic?, which is irrelevant here:
+unset CI
 lein with-profile -user,-dev inline-deps
 # Leave `git status` clean for local development:
 git checkout project.clj
