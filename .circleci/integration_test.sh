7cab5fb9f01d2c681f705db571dbb9e59e5b8c9b
vemv <vemv@users.noreply.github.com>
2021-10-23 09:03:30 +0200

    Add an e2e test against refactor-nrepl

--- .circleci/integration_test.sh
+++ .circleci/integration_test.sh
@@ -2,20 +2,31 @@
 set -Eeuxo pipefail
 cd "${BASH_SOURCE%/*}"
 
-# An integration test that exercises that cider-nrepl can successfully use mranderson @ latest.
+# An integration test that exercises that cider-nrepl and refactor-nrepl can successfully use mranderson @ latest.
+# For that, we both build those projects with mranderson in them, and run their mrandersonized test suites.
 
 cd ..
 make install
 git submodule update --init --recursive
+
+# cider-nrepl observes CI, which triggers :pedantic?, which is irrelevant here:
+unset CI
 
 cd .circleci/cider-nrepl
 lein clean
 # Undo the patch if it was applied already:
 git checkout project.clj
 git apply ../update-mranderson.patch
-# cider-nrepl observes CI, which triggers :pedantic?, which is irrelevant here:
-unset CI
 lein with-profile -user,-dev inline-deps
 lein with-profile -user,-dev,+1.10,+test,+plugin.mranderson/config test
+# Leave `git status` clean for local development:
+git checkout project.clj
+
+cd ../refactor-nrepl
+lein clean
+# Undo the patch if it was applied already:
+git checkout project.clj
+git apply ../update-mranderson-in-refactor-nrepl.patch
+make test
 # Leave `git status` clean for local development:
 git checkout project.clj
